0d333ff9a2f4e788b016e44d4d933952bb52f937
Sujin <kgj1212_@naver.com>
2020-12-17 15:42:14 +0900

    useState

--- README.md
+++ README.md
@@ -4,3 +4,21 @@ npm start
 ## Data Binding이 쉬운 매체
 * 서버에서 데이터를 받아 뿌려주는 작업 
 * react, angular, vue
+
+## ES6 destructuring 문법
+* var [ a, b ] = [ 10, 100 ]
+  * a와 b변수 순서대로 각각 10, 100 
+* let [title, setTitle] =  useState('리액트 이야기1');
+
+### 변수보다 useState를 사용하는 이유
+* 변수로 사용할 경우 변수가 바뀌변 새로고침이 됨
+* useState는 새로고침 없이 렌더링 됨
+* webapp 을 만드는 가장 중요한 이유
+
+## onClick 
+* onClick = { ()=>{} }
+* onClick = { 함수 }  / 함수() x
+
+## 필요한 부분만 복사본 만들어서 수정
+* var newArray = title;
+* newArray[0] = '수정할 부분 수정';
--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [title, setTitle] = useState(['리액트 이야기1', '리액트 이야기2', '리액트 이야기3']);
+  let [content, setContent] = useState(['12월 12일 발행', '12월 13일 발행', '12월 14일 발행',]);
+  let [addLike, setaddLike] = useState(1);
+  // let posts = '리액트 이야기1';
+
+  function changeTitle() {
+    var newArray = [...title]; //deep copy, var newArray = title은 '공유'의 의미 
+    newArray[0] = '리액트 공부하기1';
+    setTitle(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
+      </div>
+      <div>
+        <button onClick={ changeTitle }>수정</button>
+      </div>
+      <div className="list">
+        <strong>{ title[0] } <span className="like" onClick={ () => { setaddLike(addLike + 1) } }>❤️</span><em>{addLike}</em></strong>
+        <p>{ content[0] }</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <strong>{ title[1] }</strong>
+        <p>{ content[1] }</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <strong>{ title[2] }</strong>
+        <p>{ content[2] }</p>
+        <hr/>
       </div>
     </div>
   );
